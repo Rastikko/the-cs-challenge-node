a0ba774c4e260db0f2ea078bc647a47094d7985b
Johan <rastikko@gmail.com>
2017-01-07 14:51:22 -0800

    Adding a fake host to see if heroku will work

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const serviceAccount = (isHeroku) ? require("./serviceaccountheroku.js") : requi
 console.log('Is heroku: ', isHeroku);
 console.log('ServiceAccount: ', serviceAccount);
 
+var http = require('http');
+
+//Lets define a port we want to listen to
+const PORT = process.env.PORT || 8080; 
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
+
 firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert(serviceAccount),
   databaseURL: "https://the-cs-challenge.firebaseio.com"
